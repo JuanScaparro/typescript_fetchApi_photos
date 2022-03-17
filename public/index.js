f8ab459d2161f925f94d7f572089f64c7aabd6e7
JuanScaparro <juanscaparro@gmail.com>
2022-03-17 15:38:38 -0300

    Feat: search() / buildPage()

--- public/index.js
+++ public/index.js
@@ -9,8 +9,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 import { PB_API_KEY, PB_URL } from "./utils/api.js";
 let isLoading = false;
-let photos = [];
-let actualPhotoIndex = 0;
+let originalPhotos = [];
+let sourcePhotos = [];
+const quantity = 8;
+let currentPage = 1;
 const rowElement = document.querySelector('#row');
 const btnNext = document.getElementById('next');
 btnNext.addEventListener('click', next);
@@ -18,11 +20,16 @@ const btnPrev = document.querySelector('#prev');
 btnPrev.addEventListener('click', prev);
 const spinner = document.getElementById('spinner');
 const container = document.querySelector('.container');
+const pageLabel = document.getElementById('pageLabel');
+const inputSearch = document.getElementById('inputSearch');
+const btnSearch = document.getElementById('btnSearch');
+btnSearch.addEventListener('click', search);
 function loadingStatus() {
     isLoading = !isLoading;
-    spinner.style.display = isLoading ? "block" : "none";
-    container.style.display = !isLoading ? "block" : "none";
+    spinner.style.display = isLoading ? 'block' : 'none';
+    container.style.display = !isLoading ? 'block' : 'none';
 }
+;
 function loadData() {
     return __awaiter(this, void 0, void 0, function* () {
         const url = `${PB_URL.base}?key=${PB_API_KEY}&q=`;
@@ -38,24 +45,55 @@ function loadData() {
         finally {
             loadingStatus();
         }
+        ;
     });
 }
+;
 function showError(error) {
-    window.alert("UPSSSSS... te invitamos a que vuelvas a intentarlo.");
+    window.alert('UPSSSSS... te invitamos a que vuelvas a intentarlo.');
 }
+;
+// Finder
+function search(e) {
+    e.preventDefault();
+    const text = inputSearch.value.trim();
+    if (text === '') {
+        return;
+    }
+    ;
+    inputSearch.value = "";
+    const filteredPhotos = sourcePhotos.filter(photo => photo.tags.includes(text));
+    if (filteredPhotos.length === 0) {
+        window.alert('No se encontro resultado de busqueda');
+        return;
+    }
+    ;
+    sourcePhotos = filteredPhotos;
+    resetGallery();
+    buildPage();
+}
+;
 function saveData(data) {
-    photos = data;
-    buildPhotoCard(photos[actualPhotoIndex]);
+    originalPhotos = data;
+    sourcePhotos = originalPhotos;
+    buildPage();
+}
+;
+function buildPage() {
+    let pages = sourcePhotos.slice(currentPage * quantity - quantity, currentPage * quantity);
+    pageLabel.textContent = `${currentPage.toString()} / ${Math.ceil(sourcePhotos.length / quantity)}`;
+    pages.forEach(photo => {
+        buildPhotoCard(photo);
+    });
 }
 ;
 function buildPhotoCard(photo) {
     const { previewURL, tags, user, type, largeImageURL } = photo;
     const text = `User: ${user} - Type: ${type}`;
-    rowElement.innerHTML = '';
-    const template = `<div class="card text-center">
+    const template = `<div class="card text-center m-2">
                               <div class="card-body">
                                 <a href=${largeImageURL} target="_blank">
-                                 <img src=${previewURL} class="card-img-top img" alt="...">
+                                 <img src=${previewURL} class="card-img-top img-thumbnail" alt="...">
                                 </a>
                                 <h3 class="card-title">${tags}</h3>
                                 <p class="card-text">${text}</p>
@@ -65,24 +103,30 @@ function buildPhotoCard(photo) {
     div.innerHTML = template;
     rowElement.appendChild(div);
 }
+;
+function resetGallery() {
+    rowElement.innerHTML = '';
+}
 function next() {
-    if (actualPhotoIndex < photos.length) {
-        actualPhotoIndex++;
-        buildPhotoCard(photos[actualPhotoIndex]);
+    if (currentPage < Math.ceil(sourcePhotos.length / quantity)) {
+        currentPage++;
+        resetGallery();
+        buildPage();
     }
     else {
-        window.alert('Ultima Imagen');
+        window.alert('Ultima página');
     }
     ;
 }
 ;
 function prev() {
-    if (actualPhotoIndex > 0) {
-        actualPhotoIndex--;
-        buildPhotoCard(photos[actualPhotoIndex]);
+    if (currentPage > 1) {
+        currentPage--;
+        resetGallery();
+        buildPage();
     }
     else {
-        window.alert('Primer Imagen');
+        window.alert('Primer página');
     }
     ;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,121 @@
-//import { IPhoto } from "./interfaces/iPhoto.interface.js";
-import { IPizzaPhotos, IPhoto } from "./interfaces/iPizzaPhotos.interface.js";
+import { IPhoto } from "./interfaces/iPizzaPhotos.interface.js";
 import { PB_API_KEY, PB_URL } from "./utils/api.js";
 
 
-let isLoading: boolean = false
-let photos: IPhoto[] = [];
-let actualPhotoIndex: number = 0;
+let isLoading: boolean = false;
+let originalPhotos: IPhoto[] = [];
+let sourcePhotos: IPhoto[] = [];
+const quantity: number = 8;
+let currentPage: number = 1;
+
 const rowElement: HTMLElement = <HTMLElement>document.querySelector( '#row' );
-const btnNext: HTMLButtonElement = <HTMLButtonElement>document.getElementById( 'next' )
-btnNext.addEventListener( 'click', next )
-const btnPrev: HTMLButtonElement = <HTMLButtonElement>document.querySelector( '#prev' )
-btnPrev.addEventListener( 'click', prev )
-const spinner: HTMLElement = <HTMLElement>document.getElementById('spinner')
-const container: HTMLElement = <HTMLElement>document.querySelector('.container')
+const btnNext: HTMLButtonElement = <HTMLButtonElement>document.getElementById( 'next' );
+btnNext.addEventListener( 'click', next );
+const btnPrev: HTMLButtonElement = <HTMLButtonElement>document.querySelector( '#prev' );
+btnPrev.addEventListener( 'click', prev );
+const spinner: HTMLElement = <HTMLElement>document.getElementById( 'spinner' );
+const container: HTMLElement = <HTMLElement>document.querySelector( '.container' );
+const pageLabel: HTMLElement = <HTMLElement>document.getElementById( 'pageLabel' );
+const inputSearch: HTMLInputElement = <HTMLInputElement>document.getElementById( 'inputSearch' );
+const btnSearch: HTMLButtonElement = <HTMLButtonElement>document.getElementById( 'btnSearch' );
+btnSearch.addEventListener( 'click', search );
+
 
 function loadingStatus(){
-  isLoading = !isLoading
-  spinner.style.display = isLoading ? "block" : "none"
-  container.style.display = !isLoading ? "block" : "none"
-}
+  isLoading = !isLoading;
+  spinner.style.display = isLoading ? 'block' : 'none';
+  container.style.display = !isLoading ? 'block' : 'none';
+};
 
 async function loadData(){
-  const url = `${PB_URL.base}?key=${PB_API_KEY}&q=`;
-  loadingStatus()
+  const url = `${ PB_URL.base }?key=${ PB_API_KEY }&q=`;
+  loadingStatus();
     try {
       const resp = await fetch( url )
       const data = await resp.json()
-      saveData(data.hits)
-    } catch (error: any) {
-      showError(error)
+      saveData( data.hits );
+    } catch ( error: any ) {
+      showError( error );
     } finally{
-      loadingStatus()
-    }
-}
-
-function showError(error: any){
-  window.alert("UPSSSSS... te invitamos a que vuelvas a intentarlo.")
-}
-
-function saveData( data: IPhoto[] ) {
-  photos = data;
-  buildPhotoCard( photos[actualPhotoIndex] )
+      loadingStatus();
+    };
 };
 
-function buildPhotoCard( photo: IPhoto) {
-  const { previewURL, tags, user, type, largeImageURL } = photo
-  const text = `User: ${user} - Type: ${type}`
-  rowElement.innerHTML = '';
-  const template: string = `<div class="card text-center">
+function showError( error: any ){
+  window.alert( 'UPSSSSS... te invitamos a que vuelvas a intentarlo.' );
+};
+
+// Finder
+function search(e: any) {
+  e.preventDefault();
+  const text: string = inputSearch.value.trim();
+  if( text === '' ){
+    return;
+  };
+  inputSearch.value = "";
+  const filteredPhotos: IPhoto[] = sourcePhotos.filter( photo => photo.tags.includes( text ) );
+  if( filteredPhotos.length === 0 ){
+    window.alert( 'No se encontro resultado de busqueda' );
+    return;
+  };
+  sourcePhotos = filteredPhotos;
+  resetGallery();
+  buildPage();
+};
+
+
+function saveData( data: IPhoto[] ) {
+  originalPhotos = data;
+  sourcePhotos = originalPhotos;
+  buildPage();
+};
+
+function buildPage() {
+  let pages = sourcePhotos.slice( currentPage*quantity-quantity, currentPage*quantity )
+  pageLabel.textContent = `${ currentPage.toString() } / ${ Math.ceil( sourcePhotos.length / quantity ) }`;
+  pages.forEach( photo => {
+    buildPhotoCard( photo );
+  });
+};
+
+function buildPhotoCard( photo: IPhoto ) {
+  const { previewURL, tags, user, type, largeImageURL } = photo;
+  const text = `User: ${ user } - Type: ${ type }`;
+  const template: string = `<div class="card text-center m-2">
                               <div class="card-body">
-                                <a href=${largeImageURL} target="_blank">
-                                 <img src=${previewURL} class="card-img-top img" alt="...">
+                                <a href=${ largeImageURL } target="_blank">
+                                 <img src=${ previewURL } class="card-img-top img-thumbnail" alt="...">
                                 </a>
-                                <h3 class="card-title">${tags}</h3>
-                                <p class="card-text">${text}</p>
+                                <h3 class="card-title">${ tags }</h3>
+                                <p class="card-text">${ text }</p>
                               </div>
-                            </div>`
+                            </div>`;
   const div = document.createElement( 'div' );
   div.innerHTML = template;
   rowElement.appendChild( div );
+};
+
+function resetGallery() {
+  rowElement.innerHTML = '';
 }
 
 function next() {
-  if( actualPhotoIndex < photos.length ) {
-    actualPhotoIndex ++;
-    buildPhotoCard( photos[actualPhotoIndex] )
+  if( currentPage < Math.ceil( sourcePhotos.length / quantity ) ) {
+    currentPage ++;
+    resetGallery();
+    buildPage();
   }else{
-    window.alert( 'Ultima Imagen' );
+    window.alert( 'Ultima página' );
   };
 };
 
 function prev() {
-  if( actualPhotoIndex > 0 ){
-    actualPhotoIndex --;
-    buildPhotoCard( photos[actualPhotoIndex] )
+  if( currentPage > 1 ){
+    currentPage --;
+    resetGallery();
+    buildPage();
   }else{
-    window.alert( 'Primer Imagen' );
+    window.alert( 'Primer página' );
   };
 };
 
